3588fddf463db18537dc7952782ef4e40bd7c577
Sujeet <skumar.contractor@libertyglobal.com>
2023-02-04 13:54:40 +0100

    [JavaJourney][Add] Added new project to repo

---
+++ src/com/java/source/ContactsApp.java
@@ -0,0 +1,23 @@
+package com.java.source;
+
+import java.util.List;
+
+public class ContactsApp {
+	/* This application is to create a contacts application similar to mobile phones
+	 * 1. User data should be taken as input i.e First & Last name, number, mail id, nick name, DOB
+	 * 2. Name & number should be a mandatory field
+	 * 3. Validation check should be done for the data entered by the user to ensure authenticity
+	 * 4. Once user enters the data, the data should be saved into the DB
+	 * 5. User should have provision to modify the details
+	 * 6. User should be able to search data based on partial or complete name, number and nick name
+	 * 7. User should be able to delete the data
+	 * 8. Ensure exceptions are handled  
+	 * 9. Use MVC pattern and implement functions and classes to ensure code neatness.
+	 * */
+	
+	
+	public static void main (String[] args) {
+		System.out.println("Please select what you would like to do\n");
+		
+	}
+}
